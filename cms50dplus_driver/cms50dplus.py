3d70cfb61ac0311553da45fc52271380e508c2aa
Andreas <Andreas.Zachariae@gmail.com>
2024-03-11 16:06:33 +0100

    Restructure as a python class object

--- cms50dplus_driver/cms50dplus.py
+++ cms50dplus_driver/cms50dplus.py
@@ -1,160 +1,135 @@
+#!/usr/bin/env python3
 import sys
 import serial
-import calendar;
-import time;
-import signal
+import time
 import sys
-import os
-from threading import Timer
+import argparse
+
+from repeated_timer import RepeatedTimer
 
 # Decoded with datasheet https://www.tranzoa.net/~alex/blog/wp-content/uploads/2018/08/CMS60DWCommunication-protocol-of-pulse-oximeter-V7.0.pdf
 
 # 0x7D is package type for downlink commands to the device
 # 0xA1 ask for continous real-time data
 # 0x80 is unused data
-cmd_query = b'\x7d\x81\xa1\x80\x80\x80\x80\x80\x80'
+CMD_QUERY = b'\x7d\x81\xa1\x80\x80\x80\x80\x80\x80'
 
 # 0xAF Inform device beeing connected (every 5 seconds)
-cmd_maintain = b'\x7d\x81\xaf\x80\x80\x80\x80\x80\x80'
+# CMD_MAINTAIN = b'\x7d\x81\xaf\x80\x80\x80\x80\x80\x80'
 
-class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer     = None
-        self.interval   = interval
-        self.function   = function
-        self.args       = args
-        self.kwargs     = kwargs
-        self.is_running = False
-        self.start()
 
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
-
-def signal_handler(sig, frame):
-    tf = calendar.timegm(time.gmtime())
-    print(tf-ts, ' Secondes recorded !')
-    global rt
-    global file
-    rt.stop()
-    with open(file, "a") as myfile:
-        myfile.write(str(tf))
-    sys.exit(0)
-
-def send_query():
-    if serial_connection.is_open:
-        serial_connection.write(cmd_query)
-        print("sending data query")
-    else:
+class CMS50DPlus:
+    def __init__(self, serialport):
         try:
-            serial_connection.open()
-            time.sleep(0.1)
-            serial_connection.flush()
-            print('Connected: '+ str(serial_connection.is_open))
+            self.serial_connection = serial.Serial(serialport,
+                                                   baudrate=115200,
+                                                   timeout=2,
+                                                   xonxoff=1,
+                                                   bytesize=serial.EIGHTBITS,
+                                                   stopbits=serial.STOPBITS_ONE,
+                                                   parity=serial.PARITY_NONE)
+
+            print("Port: " + serialport, "Baudrate: "+str(self.serial_connection.baudrate),
+                  'Connected: ' + str(self.serial_connection.is_open))
         except OSError as err:
-            print("Serial connection is closed ", str(err))
-            print("Device was disconnected")
-            rt.stop()
+            print("Cant open specified port: "+str(err))
+            print("Device is not connected or the port is wrong")
+            sys.exit(1)
 
-def decode_data(data):
-    #### DIFFERS FROM THE DOCUMENTATION
-    bytes_hex = [data.hex()[i:i+2] for i in range(0 ,len(data.hex()) ,2)]
-    bytes_binary = [bin(int(hex_value, 16))[2:].zfill(8) for hex_value in bytes_hex]
+        self.timer = RepeatedTimer(5, self.send_query)  # send cmd_maintain every 5sec to maintain the stream session
+        print("Recording...\n^C to stop.")
 
-    # print(bytes_hex)
-    # print(bytes_binary)
-    # print([int(str(byte),2) for byte in bytes_binary])
+    def loop(self):
+        try:
+            data = self.serial_connection.read(9)  # read each 9-byte package
+        except serial.SerialException:
+            print("Trying to reconnect...")
+            self.serial_connection.close()
+            time.sleep(1)
+            self.timer.start()
+            return
+        self.decode_data(data)
 
-    if len(bytes_hex) == 0:
-        print("ERROR: No data recieved, Probably device is not turned on")
-        return
+    def send_query(self):
+        if self.serial_connection.is_open:
+            self.serial_connection.write(CMD_QUERY)
+            print("sending data query")
+        else:
+            try:
+                self.serial_connection.open()
+                time.sleep(0.1)
+                self.serial_connection.flush()
+                print('Connected: ' + str(self.serial_connection.is_open))
+            except OSError as err:
+                print("Serial connection is closed ", str(err))
+                print("Device was disconnected")
+                self.timer.stop()
 
-    if len(bytes_hex) != 9:
-        print("ERROR: Invalid data length:", len(bytes_hex))
-        return
-    
-    # first byte, real-time data package must be 0x01 = 00000001
-    if bytes_binary[0] != '00000001':
-        print("ERROR: No real-time data package")
-        return
+    def decode_data(self, data):
+        # DIFFERS FROM THE DOCUMENTATION
+        bytes_hex = [data.hex()[i:i+2] for i in range(0, len(data.hex()), 2)]
+        bytes_binary = [bin(int(hex_value, 16))[2:].zfill(8) for hex_value in bytes_hex]
 
-    # following bytes have always bit7 set to 1 = 1xxxxxxx
-    # second byte, status infos
-    if bytes_binary[1][7] == '1':
-        print("WARN: Finger is not in the device")
-        return
-    if bytes_binary[1][6] == '1':
-        print("pulse beep sound")
-    if bytes_binary[1][5] == '1':
-        print("dropping of SpO2")
-    if bytes_binary[1][4] == '1':
-        print("searching time too long")
-    signal_strength = min(int(str(bytes_binary[0])[1:4],2), 8) # if larger than 8, default to 8
+        # print(bytes_hex)
+        # print(bytes_binary)
+        # print([int(str(byte),2) for byte in bytes_binary])
 
-    # fourth byte, waveform data
-    wave = int(str(bytes_binary[3])[1:8], 2)
+        if len(bytes_hex) == 0:
+            print("ERROR: No data recieved, Probably device is not turned on")
+            return
 
-    # sixth byte, pulse rate value 
-    bpm = int(str(bytes_binary[5])[1:8], 2)
+        if len(bytes_hex) != 9:
+            print("ERROR: Invalid data length:", len(bytes_hex))
+            return
 
-    # seventh byte, spo2 value
-    spo2 = int(str(bytes_binary[6])[1:8], 2)
-    if spo2 > 100:
-        print("SpO2 is invalid")
-        spo2 = -1
+        # first byte, real-time data package must be 0x01 = 00000001
+        if bytes_binary[0] != '00000001':
+            print("ERROR: No real-time data package")
+            return
 
-    print(bpm, spo2, wave)
+        # following bytes have always bit7 set to 1 = 1xxxxxxx
+        # second byte, status infos
+        if bytes_binary[1][7] == '1':
+            print("WARN: Finger is not in the device")
+            return
+        if bytes_binary[1][6] == '1':
+            print("pulse beep sound")
+        if bytes_binary[1][5] == '1':
+            print("dropping of SpO2")
+        if bytes_binary[1][4] == '1':
+            print("searching time too long")
+        signal_strength = min(int(str(bytes_binary[0])[1:4], 2), 8)  # if larger than 8, default to 8
+
+        # fourth byte, waveform data
+        wave = int(str(bytes_binary[3])[1:8], 2)
+
+        # sixth byte, pulse rate value
+        bpm = int(str(bytes_binary[5])[1:8], 2)
+
+        # seventh byte, spo2 value
+        spo2 = int(str(bytes_binary[6])[1:8], 2)
+        if spo2 > 100:
+            print("SpO2 is invalid")
+            spo2 = -1
+
+        print(bpm, spo2, wave)
 
 
-signal.signal(signal.SIGINT, signal_handler)
-if os.path.exists(sys.argv[2]):
-    os.remove(sys.argv[2])
+def main():
+    parser = argparse.ArgumentParser(description="cms50dplus.py - A driver for the Contec CMS50D+ pulse oximeter.")
+    parser.add_argument("serialport", help="The device's virtual serial port.")
+    args = parser.parse_args()
 
-try:
-    serial_connection = serial.Serial(sys.argv[1],
-                        baudrate=115200, #in my device, this is the baudrate where to open the serial, in other devices try 4800, 19200...
-                        timeout=2,
-                        xonxoff=1,
-                        bytesize=serial.EIGHTBITS,
-                        stopbits=serial.STOPBITS_ONE,
-                        parity=serial.PARITY_NONE) #another important looking like parameter, setting the parity to NONE
+    driver = CMS50DPlus(args.serialport)
 
-    print("Port: "+sys.argv[1], "Baudrate: "+str(serial_connection.baudrate), 'Connected: '+ str(serial_connection.is_open))
-except OSError as err:
-    print("Cant open specified port: "+str(err))
-    print("Device is not connected or the port is wrong")
-    sys.exit(1)
+    while True:
+        try:
+            driver.loop()
+        except KeyboardInterrupt:
+            driver.timer.stop()
+            print('Recording stopped.')
+            break
 
-# serial_connection.write(cmd_query)
 
-file = sys.argv[2]
-rt = RepeatedTimer(5, send_query) #send cmd_maintain every 5sec to maintain the stream session
-print("Recording...\n^C to stop.")
-
-ts = calendar.timegm(time.gmtime()) #10 characters to skip when decoding data
-with open(sys.argv[2], "a") as myfile:
-        myfile.write(str(ts))
-while True:
-    try:
-        data = serial_connection.read(9) #read each byte
-    except serial.SerialException:
-        print("Trying to reconnect...")
-        serial_connection.close()
-        time.sleep(1)
-        rt.start()
-        continue
-    decode_data(data)
-    # print(data)
-    with open(sys.argv[2], "a") as myfile:
-        myfile.write(data.hex())
+if __name__ == "__main__":
+    main()
